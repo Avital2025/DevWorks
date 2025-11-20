c3970f07290f02dd0fcb181a097082e2ef9265af
Avital2025 <A0583273344@gmail.com>
2025-11-20 22:06:56 +0200

    update_hostages

--- React/Employers/src/Components/AppLayuot.tsx
+++ React/Employers/src/Components/AppLayuot.tsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-import HostagesCounter from "./HostagesCounter";
+// import HostagesCounter from "./HostagesCounter";
 import ChatBot from "./ChatBot";
 
 export default function Layout() {
   return (
     <div>
       <NavBar />
-      <HostagesCounter />
+      {/* <HostagesCounter /> */}
       <div style={{  marginBottom: "100px" }}>
         <Outlet />
       </div>
